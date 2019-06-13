eaf1f4b396daedc93f9aa81cdb5bb46f03f17ae7
Spencer Stock <spencerstock@gmail.com>
2019-06-13 14:45:19 -0600

    initial commit

---
+++ src/test/java/local/sgs/javazoos/JavaZoosApplicationTests.java
@@ -0,0 +1,16 @@
+package local.sgs.javazoos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaZoosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
